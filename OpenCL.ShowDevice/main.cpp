6366a3dc8fbfbcdeddc3ed218a4f0266e854f820
Peter.Li <yeshuanova@gmail.com>
2015-06-07 14:16:48 +0800

    Initial Commit

---
+++ OpenCL.ShowDevice/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OpenCL.ShowDevice
+//
+//  Created by Peter.Li on 2015/6/7.
+//  Copyright (c) 2015年 Peter.Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    std::cout << "Hello, World!\n";
+
+    
+    return 0;
+}
